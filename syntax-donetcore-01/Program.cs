b1866d2d70b3787eb7306e029b4ebfa79e93acf2
DESKTOP-7DAIUF4\viet tran <vtran@yourcause.com>
2018-01-29 15:23:22 -0600

    init project

---
+++ syntax-donetcore-01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace syntax_donetcore_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
