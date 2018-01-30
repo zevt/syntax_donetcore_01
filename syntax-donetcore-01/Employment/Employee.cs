e9a411d30a71148de0440b24a2c2be51a9c0035a
DESKTOP-7DAIUF4\viet tran <vtran@yourcause.com>
2018-01-30 10:48:10 -0600

    add Employee class

---
+++ syntax-donetcore-01/Employment/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace Experiment.Employment
+{
+    internal class Employee
+    {
+        private string _department;
+        private int _empID;
+        private string _loginName;
+        private string _name;
+        private string _password;
+
+        public string Name
+        {
+            set { _name = value; }
+            get { return _name;  }
+        }
+
+
+    }
+}
